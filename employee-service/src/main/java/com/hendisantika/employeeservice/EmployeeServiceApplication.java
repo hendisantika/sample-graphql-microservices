81466ca4ac3422e4f82e377332bd5147d282e767
Hendi Santika <hendisantika@gmail.com>
2018-11-08 06:36:52 +0700

    #18 - Add module employee service

---
+++ employee-service/src/main/java/com/hendisantika/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+}
